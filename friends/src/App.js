2db63bad89e3c6736ca752f983a4697a4fd50df6
sofialevin <slevin@M-4876.local>
2019-10-07 10:04:58 -0700

    MVP and stretch.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, withRouter, Redirect, Link } from "react-router-dom";
+
+import Login from './components/Login';
+import FriendsList from './components/FriendsList';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 function App() {
+
+  const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={(props) => (
+      localStorage.getItem("token")
+        ? <Component {...props} {...rest}/>
+        : <Redirect to='/login' />
+    )} />
+    )
+
+    const handleLogout = () => {
+      localStorage.removeItem('token');
+    }
+
   return (
     <div className="App">
+      <Navbar bg="light justify-content-between">
+        <Navbar.Brand>My Awesome Friends</Navbar.Brand>
+        {localStorage.getItem("token") ? 
+        <Nav>
+          <Navbar.Text>
+            <Link to="/login" onClick={handleLogout}>Logout</Link>
+          </Navbar.Text>
+        </Nav> : <div></div> }
+      </Navbar>
+      <Route path="/login" component={Login} />
+      <PrivateRoute path="/dashboard" component={FriendsList}/>
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
